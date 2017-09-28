6bcd57a6cf36f02afbbf6a662c437886ade95da5
Ryan Williams <rwilliams5262@gmail.com>
2017-09-28 14:12:07 -0400

    Adding methods to job template class to create and delete templates

--- awx/awx.py
+++ awx/awx.py
@@ -298,6 +298,84 @@ class AwxJobTemplate(AwxBase):
 
     def __init__(self):
         super(AwxJobTemplate, self).__init__()
+        self._inventory = AwxInventory()
+        self._credential = AwxCredential()
+        self._project = AwxProject()
+
+    @property
+    def project(self):
+        """Return project instance."""
+        return self._project
+
+    @property
+    def credential(self):
+        """Return credential instance."""
+        return self._credential
+
+    @property
+    def inventory(self):
+        """Return inventory instance."""
+        return self._inventory
+
+    @property
+    def job_templates(self):
+        """Return a list of job templates."""
+        return self.resource.list()
+
+    def create(self, name, description, job_type, inventory, project, playbook,
+               credential):
+        """Create a job template.
+
+        :param name: Template name.
+        :type name: str
+        :param description: Template description.
+        :type description: str
+        :param job_type: Job type.
+        :type job_type: str
+        :param inventory: Inventory name.
+        :type inventory: str
+        :param project: Project name.
+        :type project: str
+        :param playbook: Playbook name.
+        :type playbook: str
+        :param credential: Credential name.
+        :type credential: str
+        """
+        # get credential object
+        _credential = self.credential.get(credential)
+
+        # get inventory object
+        _inventory = self.inventory.get(inventory)
+
+        # get project object
+        _project = self.project.get(project)
+
+        try:
+            self.resource.create(
+                name=name,
+                description=description,
+                job_type=job_type,
+                inventory=_inventory['id'],
+                project=_project['id'],
+                playbook=playbook,
+                credential=_credential['id']
+            )
+        except Found as ex:
+            raise Exception(ex.message)
+
+    def delete(self, name, project):
+        """Delete a job template.
+
+        :param name: Template name.
+        :type name: str
+        :param project: Project name.
+        :type project: str
+        """
+        # get project object
+        _project = self.project.get(project)
+
+        # delete job template
+        self.resource.delete(name=name, project=_project['id'])
 
 
 class AwxLabel(AwxBase):
