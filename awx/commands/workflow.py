1480f8bab0398eef240033a605727c545bcbfd99
vi-patel <vipatel@redhat.com>
2017-10-25 10:00:23 -0400

    adding updates to support a workflow (#13)

--- awx/commands/workflow.py
+++ awx/commands/workflow.py
@@ -1,5 +1,7 @@
 """Awx workflow helper module."""
 from ..base import AwxBase
+from .organization import AwxOrganization
+from tower_cli.exceptions import Found, NotFound
 
 # TODO: Add in additional parameters that are optional for all methods.
 
@@ -11,3 +13,105 @@ class AwxWorkflow(AwxBase):
     def __init__(self):
         """Constructor."""
         super(AwxWorkflow, self).__init__()
+        self._organization = AwxOrganization()
+
+    @property
+    def organization(self):
+        """Return organization instance."""
+        return self._organization
+
+    @property
+    def workflow_templates(self):
+        """Return a list of workflow templates."""
+        return self.resource.list()
+
+    def create(self, name, description, organization, fail_on_found=True):
+        """Create a workflow template.
+
+        :param name: Workflow template name.
+        :type name: str
+        :param description: Workflow template description.
+        :type description: str
+        :param organization: Orgnaization of the workflow template.
+        :type organization: str
+        :param fail_on_found: option to fail if found.
+        :type fail_on_found: boolean
+        """
+
+        self.logger.info('Creating workflow template %s.' % name)
+        _org = self.organization.get(organization)
+
+        # quit if organization not found
+        if not _org:
+            raise Exception('Organization %s not found.' % organization)
+
+        try:
+            self.resource.create(
+                name=name,
+                description=description,
+                organization=_org['id'],
+                fail_on_found=fail_on_found
+            )
+        except Found as ex:
+            self.logger.error('Workflow template %s already exists!' % name)
+            raise Exception(ex.message)
+
+        self.logger.info('Workflow template %s successfully created!' % name)
+
+    def upload_schema(self, name, schema_loc):
+        """Upload a workflow schema (json or yaml)
+
+        :param name: Workflow template name.
+        :type name: str
+        :param schema_loc: File location of the schema.
+        :type schema_loc: str
+        :return: workflow template object.
+        :rtype: dict
+        """
+        workflow_obj = self.get(name)
+        # quit if organization not found
+        if not workflow_obj:
+            raise Exception('Workflow Template {} not found.'.format(name))
+        return self.resource.schema(wfjt=workflow_obj["id"],
+                                    node_network=schema_loc)
+
+    def get_schema(self, name):
+        """Get the schema of an existing workflow schema
+
+        :param name: Workflow template name.
+        :type name: str
+        :return: workflow template object.
+        :param format: format of the output
+        :type: enum (yaml, json, human)
+        :rtype: dict
+        """
+        workflow_obj = self.get(name)
+        # quit if organization not found
+        if not workflow_obj:
+            raise Exception('Workflow Template {} not found.'.format(name))
+        return self.resource.schema(wfjt=workflow_obj["id"])
+
+    def delete(self, name):
+        """Delete a workflow template.
+
+        :param name: Workflow template name.
+        :type name: str
+        """
+
+        # delete workflow template
+        self.logger.info('Deleting workflow template %s.' % name)
+        self.resource.delete(name=name)
+        self.logger.info('Workflow template %s successfully deleted!' % name)
+
+    def get(self, name):
+        """Get the workflow
+
+        :param name: Workflow template name.
+        :type name: str
+        :return: Workflow object.
+        :type dict
+        """
+        try:
+            return self.resource.get(name=name)
+        except NotFound as ex:
+            raise Exception(ex.message)
--- awx/commands/workflow_job.py
+++ awx/commands/workflow_job.py
@@ -1,5 +1,8 @@
 """Awx workflow job helper module."""
+import json
+from tower_cli.exceptions import NotFound
 from ..base import AwxBase
+from .workflow import AwxWorkflow
 
 # TODO: Add in additional parameters that are optional for all methods.
 
@@ -11,3 +14,123 @@ class AwxWorkflowJob(AwxBase):
     def __init__(self):
         """Constructor."""
         super(AwxWorkflowJob, self).__init__()
+        self._workflow = AwxWorkflow()
+
+    @property
+    def workflow(self):
+        """Return job template instance."""
+        return self._workflow
+
+    @property
+    def workflow_jobs(self):
+        """Return a list of jobs."""
+        return self.resource.list()
+
+    def launch(self, name, extra_vars=None):
+        """Launch a new job from a job template.
+
+        :param name: Template name.
+        :type name: str
+        :param extra_vars: Extra variables.
+        :type extra_vars: list
+        :return: Launch data
+        :rtype: dict
+        """
+        # get job template object
+        _workflow_template = self.workflow.get(name)
+
+        # set extra vars
+        _extra_vars = list()
+        if extra_vars:
+            for elem in extra_vars:
+                _extra_vars.append(json.dumps(elem))
+        else:
+            _extra_vars = None
+
+        return self.resource.launch(
+            workflow_job_template=_workflow_template['id'],
+            extra_vars=_extra_vars
+        )
+
+    def status(self, job_id):
+        """Get the job status
+
+        :param job_id: job id.
+        :type job_id: int
+        :return: status
+        :rtype: dict
+        """
+        try:
+            return self.resource.status(job_id)
+        except NotFound as ex:
+            raise Exception(ex.message)
+
+    def summary(self, job_id):
+        """Get the unified standard out
+
+        :param job_id: job id.
+        :type job_id: int
+        :return: stdout from the playbook execution
+        :rtype: dict
+        """
+        try:
+            return self.resource.summary(job_id)
+        except NotFound as ex:
+            raise Exception(ex.message)
+
+    def monitor(self, job_id, interval=1.0, timeout=3600):
+        """Monitor the job
+        by default check every second for 60 mins/hr
+
+        :param job_id: job id.
+        :type job_id: int
+        :return: job information
+        :rtype: dict
+        """
+        try:
+            return self.resource.monitor(
+                job_id,
+                interval=interval,
+                timeout=timeout
+            )
+        except NotFound as ex:
+            raise Exception(ex.message)
+
+    def cancel(self, job_id):
+        """Get the job
+
+        :param job_id: job id.
+        :type job_id: int
+        :return: job
+        :rtype: dict
+        """
+        try:
+            return self.resource.cancel(job_id)
+        except NotFound as ex:
+            raise Exception(ex.message)
+
+    def get(self, job_id):
+        """Get the job
+
+        :param job_id: job id.
+        :type job_id: int
+        :return: job
+        :rtype: dict
+        """
+        try:
+            return self.resource.get(job_id)
+        except NotFound as ex:
+            raise Exception(ex.message)
+
+    def delete(self, job_id):
+        """Get the unified standard out
+
+        :param job_id: job id.
+        :type job_id: int
+        :return: stdout from the playbook execution
+        :rtype: dict
+        """
+        try:
+            return self.resource.delete(job_id)
+        except NotFound as ex:
+            raise Exception(ex.message)
