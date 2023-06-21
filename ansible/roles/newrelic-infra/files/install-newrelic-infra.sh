13b80b0876e3e610ddefaf58c1cc240c157913d3
Zach Sifakis <zachs@oc-lt-zachs.local>
2023-06-21 14:31:40 +0300

    key

--- ansible/roles/newrelic-infra/files/install-newrelic-infra.sh
+++ ansible/roles/newrelic-infra/files/install-newrelic-infra.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "license_key: 12345" | sudo tee -a /etc/newrelic-infra.yml
-sudo curl -o /etc/yum.repos.d/newrelic-infra.repo https://download.newrelic.com/infrastructure_agent/linux/yum/amazonlinux/2023/aarch64/newrelic-infra.repo
+sudo curl -o /etc/yum.repos.d/newrelic-infra.repo https://download.newrelic.com/infrastructure_agent/linux/yum/amazonlinux/2023/x86_64/newrelic-infra.repo
 sudo yum -q makecache -y --disablerepo='*' --enablerepo='newrelic-infra'
 sudo yum install newrelic-infra -y
